1879dcd5bf0e7fcfc134b4b1232c51571f8f5ad8
Anamika <anamika.sawhney22@gmail.com>
2019-09-04 15:25:43 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ApiListCompComponent } from './api-list-comp/api-list-comp.component';
+import { StudentServiceService } from './student-service.service';
+// import { InterceptorModule } from './interceptor/interceptor.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiListCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule, ReactiveFormsModule
   ],
-  providers: [],
+  providers: [StudentServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
